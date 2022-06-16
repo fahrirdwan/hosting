601927d8ea36d5e0bc57799922a016204fab969f
fahri <fahriridwan89@gmail.com>
2022-06-16 12:53:17 +0700

    project akhir

---
+++ review/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import requests
+import sys
+from subprocess import run,PIPE
+
+def button(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def external(request):
+    inp= request.POST.get('param')
+    out= run([sys.executable,'C:/Python/CarParkProject/CarParkProject/main.py',inp],shell=False,stdout=PIPE)
+    print(out)
+
+    return render(request,'index.html',{'data1':out.stdout.decode('utf-8')})
